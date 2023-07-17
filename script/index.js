4603fcbb64fcf97e53490f201249cedcc972c5ba
Nadezhda Vorotova <work-nadezhda-v@yandex.ru>
2023-07-17 15:10:18 +0400

    Добавлены функции createRow и renderGoods для создания и отрисовки строк таблицы

--- script/index.js
+++ script/index.js
@@ -4,3 +4,71 @@ const modalTitle = document.querySelector('.modal__title');
 const modalForm = document.querySelector('.modal__form'); 
 const modalCheckbox = document.querySelector('#discount');
 const modalInputDiscount = document.querySelector('.modal__input_discount');
+const overlay = document.querySelector('.overlay');
+
+overlay.classList.remove('active');
+
+const createRow = (item) => {
+  const rowItem = document.createElement('tr');
+  const cellOrder = document.createElement('td');
+  cellOrder.className = 'table__cell';
+  cellOrder.textContent = item.id;
+
+  const cellName = document.createElement('td');
+  cellName.className = 'table__cell table__cell_left table__cell_name';
+  cellName.dataset.id = '24601654816512';
+  cellName.textContent = item.title;
+
+  const span = document.createElement('span');
+  span.className = 'table__cell-id';
+  span.textContent = `id: ${cellName.dataset.id}`;
+
+  const cellCategory = document.createElement('td');
+  cellCategory.className = 'table__cell table__cell_left';
+  cellCategory.textContent = item.category;
+
+  const cellUnits = document.createElement('td');
+  cellUnits.className = 'table__cell';
+  cellUnits.textContent = item.units;
+
+  const cellCount = document.createElement('td');
+  cellCount.className = 'table__cell';
+  cellCount.textContent = item.count;
+
+  const cellPrice = document.createElement('td');
+  cellPrice.className = 'table__cell';
+  cellPrice.textContent = '$' + item.price;
+
+  const cellSum = document.createElement('td');
+  cellSum.className = 'table__cell';
+  cellSum.textContent = '$' + item.count * item.price;
+
+  const cellButtons = document.createElement('td');
+  cellButtons.className = 'table__cell table__cell_btn-wrapper';
+
+  const buttonPic = document.createElement('button');
+  buttonPic.className = 'table__btn table__btn_pic';
+
+  const buttonEdit = document.createElement('button');
+  buttonEdit.className = 'table__btn table__btn_edit';
+
+  const buttonDel = document.createElement('button');
+  buttonDel.className = 'table__btn table__btn_del';
+
+  rowItem.append(cellOrder, cellName, cellButtons);
+  cellName.prepend(span);
+  const cells = [cellCategory, cellUnits, cellCount, cellPrice, cellSum];
+  cellName.after(...cells);
+  cellButtons.append(buttonPic, buttonEdit, buttonDel);
+
+  return rowItem;
+}
+
+const table = document.querySelector('.table__body');
+
+const renderGoods = (arr) => {
+  arr.forEach(element => {
+    const rowItem = createRow(element);
+    table.append(rowItem);
+  });
+}
