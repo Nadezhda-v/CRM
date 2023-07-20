863ea87f04ffbcb7b80a4585e9e97c0802d9600e
Nadezhda Vorotova <work-nadezhda-v@yandex.ru>
2023-07-20 20:30:49 +0400

    Добавить функциональность удаления строки из таблицы
    Исправлены ошибки линтера
    Исправлен обработчик событий при закрытии модального окна для избежания дублирования кода и обеспечения универсальности

--- script/index.js
+++ script/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const modalTitle = document.querySelector('.modal__title');
-const modalForm = document.querySelector('.modal__form'); 
+const modalForm = document.querySelector('.modal__form');
 const modalCheckbox = document.querySelector('#discount');
 const modalInputDiscount = document.querySelector('.modal__input_discount');
 const overlay = document.querySelector('.overlay');
@@ -64,7 +64,7 @@ const createRow = (item) => {
   cellButtons.append(buttonPic, buttonEdit, buttonDel);
 
   return rowItem;
-}
+};
 
 const table = document.querySelector('.table__body');
 const rowsTable = table.querySelectorAll('tr');
@@ -76,83 +76,120 @@ const renderGoods = (arr) => {
     count++;
     table.append(rowItem);
   });
-}
+};
 
-let goods = [
+const goods = [
   {
-    "id": 1,
-    "title": "Смартфон Xiaomi 11T 8/128GB",
-    "price": 27000,
-    "description": "Смартфон Xiaomi 11T – это представитель флагманской линейки, выпущенной во второй половине 2021 года. И он полностью соответствует такому позиционированию, предоставляя своим обладателям возможность пользоваться отличными камерами, ни в чем себя не ограничивать при запуске игр и других требовательных приложений.",
-    "category": "mobile-phone",
-    "discont": false,
-    "count": 3,
-    "units": "шт",
-    "images": {
-      "small": "img/smrtxiaomi11t-m.jpg",
-      "big": "img/smrtxiaomi11t-b.jpg"
-    }
+    'id': 1,
+    'title': 'Смартфон Xiaomi 11T 8/128GB',
+    'price': 27000,
+    'description':
+      `Смартфон Xiaomi 11T – это представитель флагманской линейки, 
+      выпущенной во второй половине 2021 года. И он полностью соответствует 
+      такому позиционированию, предоставляя своим обладателям возможность 
+      пользоваться отличными камерами, ни в чем себя не ограничивать при 
+      запуске игр и других требовательных приложений.`,
+    'category': 'mobile-phone',
+    'discont': false,
+    'count': 3,
+    'units': 'шт',
+    'images': {
+      'small': 'img/smrtxiaomi11t-m.jpg',
+      'big': 'img/smrtxiaomi11t-b.jpg',
+    },
   },
   {
-    "id": 2,
-    "title": "Радиоуправляемый автомобиль Cheetan",
-    "price": 4000,
-    "description": "Внедорожник на дистанционном управлении. Скорость 25км/ч. Возраст 7 - 14 лет",
-    "category": "toys",
-    "discont": 5,
-    "count": 1,
-    "units": "шт",
-    "images": {
-      "small": "img/cheetancar-m.jpg",
-      "big": "img/cheetancar-b.jpg"
-    }
+    'id': 2,
+    'title': 'Радиоуправляемый автомобиль Cheetan',
+    'price': 4000,
+    'description':
+      `Внедорожник на дистанционном управлении. 
+      Скорость 25км/ч. Возраст 7 - 14 лет`,
+    'category': 'toys',
+    'discont': 5,
+    'count': 1,
+    'units': 'шт',
+    'images': {
+      'small': 'img/cheetancar-m.jpg',
+      'big': 'img/cheetancar-b.jpg',
+    },
   },
   {
-    "id": 3,
-    "title": "ТВ приставка MECOOL KI",
-    "price": 12400,
-    "description": "Всего лишь один шаг сделает ваш телевизор умным, Быстрый и умный MECOOL KI PRO, прекрасно спроектированный, сочетает в себе прочный процессор Cortex-A53 с чипом Amlogic S905D",
-    "category": "tv-box",
-    "discont": 15,
-    "count": 4,
-    "units": "шт",
-    "images": {
-      "small": "img/tvboxmecool-m.jpg",
-      "big": "img/tvboxmecool-b.jpg"
-    }
+    'id': 3,
+    'title': 'ТВ приставка MECOOL KI',
+    'price': 12400,
+    'description':
+      `Всего лишь один шаг сделает ваш телевизор умным, 
+      Быстрый и умный MECOOL KI PRO, прекрасно спроектированный, 
+      сочетает в себе прочный процессор Cortex-A53 с чипом Amlogic S905D`,
+    'category': 'tv-box',
+    'discont': 15,
+    'count': 4,
+    'units': 'шт',
+    'images': {
+      'small': 'img/tvboxmecool-m.jpg',
+      'big': 'img/tvboxmecool-b.jpg',
+    },
   },
   {
-    "id": 4,
-    "title": "Витая пара PROConnect 01-0043-3-25",
-    "price": 22,
-    "description": "Витая пара Proconnect 01-0043-3-25 является сетевым кабелем с 4 парами проводов типа UTP, в качестве проводника в которых используется алюминий, плакированный медью CCA. Такая неэкранированная витая пара с одножильными проводами диаметром 0.50 мм широко применяется в процессе сетевых монтажных работ. С ее помощью вы сможете обеспечить развертывание локальной сети в домашних условиях или на предприятии, объединить все необходимое вам оборудование в единую сеть.",
-    "category": "cables",
-    "discont": false,
-    "count": 420,
-    "units": "v",
-    "images": {
-      "small": "img/lan_proconnect43-3-25.jpg",
-      "big": "img/lan_proconnect43-3-25-b.jpg"
-    }
-  }
-]
+    'id': 4,
+    'title': 'Витая пара PROConnect 01-0043-3-25',
+    'price': 22,
+    'description':
+      `Витая пара Proconnect 01-0043-3-25 является сетевым кабелем 
+      с 4 парами проводов типа UTP, в качестве проводника в которых 
+      используется алюминий, плакированный медью CCA. 
+      Такая неэкранированная витая пара с одножильными проводами 
+      диаметром 0.50 мм широко применяется в процессе сетевых монтажных 
+      работ. С ее помощью вы сможете обеспечить развертывание локальной 
+      сети в домашних условиях или на предприятии, 
+      объединить все необходимое вам оборудование в единую сеть.`,
+    'category': 'cables',
+    'discont': false,
+    'count': 420,
+    'units': 'v',
+    'images': {
+      'small': 'img/lan_proconnect43-3-25.jpg',
+      'big': 'img/lan_proconnect43-3-25-b.jpg',
+    },
+  },
+];
 
 renderGoods(goods);
 
 const panelAddGoods = document.querySelector('.panel__add-goods');
-const buttonModalClose = document.querySelector('.modal__close');
 const overlayModal = document.querySelector('.overlay__modal');
+
+// Открытие модального окна
 
 panelAddGoods.addEventListener('click', () => {
   overlay.classList.add('active');
 });
 
-buttonModalClose.addEventListener('click', () => {
-  overlay.classList.remove('active');
+// Закрытие модального окна
+
+overlay.addEventListener('click', e => {
+  const target = e.target;
+
+  if (!overlayModal.contains(target) || target.closest('.modal__close')) {
+    overlay.classList.remove('active');
+  }
 });
 
-document.addEventListener('click', e => {
-  if(!overlayModal.contains(e.target) && e.target !== panelAddGoods) {
-    overlay.classList.remove('active');
-  };
+// Удаление строки из таблицы
+
+table.addEventListener('click', e => {
+  const target = e.target;
+
+  if (target.closest('.table__btn_del')) {
+    const rowItem = target.closest('tr');
+    const dataId = rowItem.querySelector('.table__cell_name').dataset.id;
+    const index = goods.findIndex(item => item.id === +dataId);
+
+    goods.splice(index, 1);
+
+    rowItem.remove();
+  }
+
+  console.log(goods);
 });
